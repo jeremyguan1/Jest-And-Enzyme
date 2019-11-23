6c6409601c18879eb5ddd30627de958800efa8b8
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-22 18:11:19 -0800

    button

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -3,10 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
   render() {
     return (
       <div data-test="component-app">
-        <h1>App</h1>
+        <h1 data-test="counter-display">The Counter is currently {this.state.counter}</h1>
+        <button
+          data-test="increment-button"
+          onClick={() => this.setState(state => ({ counter: state.counter + 1 }))}
+        >
+          Increment counter
+        </button>
       </div>
     );
   }
