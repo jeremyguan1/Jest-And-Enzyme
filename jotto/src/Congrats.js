f8a5d9170c082b1e93da34d21132dcc0b9f46409
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-25 14:14:07 -0800

    good

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">Congratulations! you guessed the word!</span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats"></div>;
+  }
+};
