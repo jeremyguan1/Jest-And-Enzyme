9c92216b9ee338fca0da64ee33ee8450dff5fc05
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-25 17:37:40 -0800

    good

---
+++ jotto/src/GuessWords.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GuessWords = props => {
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = <span data-test="guess-instructions">Try to guess the secret word!</span>;
+  } else {
+    const guessWordsRows = props.guessedWords.map((word, index) => {
+      return (
+        <tr data-test="guessed-word" key={index}>
+          <td>{word.guessWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessWordsRows}</tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return <div data-test="component-guessed-words">{contents}</div>;
+};
+
+GuessWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
+
+export default GuessWords;
