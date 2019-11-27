28a91bb6229d48e5e66735959a99c0c28fa92c15
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-26 16:42:01 -0800

    good

---
+++ jotto/src/Input.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Input extends Component {
+  render() {
+    console.log(this.props);
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder={"Enter guess"}
+        />
+        <button data-test="submit-button" type="submit" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+    );
+    return (
+      <div data-test="component-input">
+        <button>{contents}</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ success }) => {
+  return { success };
+};
+
+export default connect(mapStateToProps)(Input);
